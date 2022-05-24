e61e10c7ee727cc93c47de42d0f8fc079ad7fb31
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-05-24 16:17:33 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/Home';
+import FirstScreen from './src/screens/FirstScreen';
+import SecondScreen from './src/screens/SecondScreen';
+
+export default function App() {
+  const Stack = createStackNavigator()
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          component={Home}
+          name="Home"
+        />
+        <Stack.Screen 
+          component={FirstScreen}
+          name="First"
+        />
+        <Stack.Screen 
+          component={SecondScreen}
+          name="Second"
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
